56edcb3f42caaab43d64c6d5c7d52358bf2e90e0
js395 <js395@st-andrews.ac.uk>
2019-11-22 16:20:34 +0000

    Added player setup, and terminal for choosing cards and helper functions to check if chosen card is valid

---
+++ src/Card.java
@@ -0,0 +1,32 @@
+
+
+
+public class Card {
+
+    private final String SUIT;
+    private final String RANK;
+
+    public Card (String suit, String rank) {
+        SUIT = suit;
+        RANK = rank;
+    }
+
+    @Override
+    public String toString() {
+        return this.RANK + " OF " + this.SUIT;
+    }
+
+    public String getRANK() {
+        return RANK;
+    }
+
+    public String getSUIT() {
+        return SUIT;
+    }
+
+    public boolean equals(Card card) {
+        return this.RANK.equals(card.getSUIT()) && this.RANK.equals(card.RANK);
+    }
+}
+
+
---
+++ src/Hand.java
@@ -0,0 +1,52 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class Hand {
+
+    private LinkedList<Card> hand;
+
+    public  Hand (LinkedList<Card> hand) {
+        this.hand = hand;
+    }
+    public Hand () {
+        this.hand = new LinkedList<>();
+    }
+
+    public int getHandSize () {
+        return hand.size();
+    }
+
+    public String toString() {
+        if (hand.size() == 0) return "EMPTY HAND";
+        String handString = "";
+        ListIterator<Card> iterator = hand.listIterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+            Card card = iterator.next();
+            handString += card.toString() + " [" + i + "]";
+            if (iterator.hasNext()) handString += ", ";
+            i++;
+        }
+        return handString;
+    }
+
+    public Card get(int index) {
+        return hand.get(index);
+    }
+
+    public Card giveCard (int num) {
+        return this.hand.remove(num);
+    }
+
+    public void getCard (Card card) {
+        this.hand.add(card);
+    }
+
+    public LinkedList<Card> getHand() {
+        return hand;
+    }
+
+    public void dropHand () {
+        this.hand = new LinkedList<>();
+    }
+}
