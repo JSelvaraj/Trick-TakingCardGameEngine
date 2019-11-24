554e7c3eacfac842d4bd9cd77ba5378165c2fb2e
eg207 <eg207@st-andrews.ac.uk>
2019-11-24 16:48:15 +0000

    added initial parser

--- src/main/java/src/parser/Parser.java
+++ src/main/java/src/parser/Parser.java
@@ -1,25 +1,33 @@
 package src.parser;
+
+import org.everit.json.schema.Schema;
 import org.everit.json.schema.ValidationException;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
-import org.everit.json.schema.Schema;
-import org.everit.json.schema.loader.SchemaLoader;
+import src.exceptions.InvalidGameDescriptionException;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 
-public class Parser{
+public class Parser {
     private static final String schemaFile = "json-schema.json";
     private Schema schema;
 
-    public Parser(){
+    private final String[] DEFAULT_SUITS = {"HEARTS", "CLUBS", "DIAMONDS", "SPADES"};
+    private final String[] DEFAULT_RANKS = {"ACE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "JACK", "QUEEN", "KING"};
+    private final String[] DEFAULT_RANK_ORDER = {"TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", "JACK", "QUEEN", "KING", "ACE"};
+
+    public Parser() {
         this.schema = initSchema();
     }
 
-    private Schema initSchema(){
+    private Schema initSchema() {
         try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(schemaFile)) {
-            if(inputStream == null){
+            if (inputStream == null) {
                 throw new IOException();
             }
             JSONObject rawSchema = new JSONObject(new JSONTokener(inputStream));
@@ -29,14 +37,133 @@ public class Parser{
         }
     }
 
-    public boolean validateObject(JSONObject object){
+    public boolean validateObject(JSONObject object) {
         try {
             schema.validate(object);
             return true;
-        } catch (ValidationException e){
+        } catch (ValidationException e) {
             System.out.println(e.toJSON().toString(4));
             return false;
         }
     }
+
+    public GameDesc parseGameDescription(String filename) throws InvalidGameDescriptionException {
+        JSONObject gameJSON = readJSONFile(filename);
+        if (gameJSON == null) {
+            throw new InvalidGameDescriptionException("Failed to parse game description file.")
+        }
+        try {
+            schema.validate(gameJSON);
+        } catch (ValidationException e) {
+            throw new InvalidGameDescriptionException(e.toJSON().toString(4));
+        }
+        //Gets the name and description
+        String name = gameJSON.getString("name");
+        String description = gameJSON.getString("description");
+        //Gets player count
+        int players = gameJSON.getInt("players");
+        //Gets the suits and ranks to make the deck from.
+        String[] suits;
+        String[] ranks;
+        String[] rank_order;
+        if (gameJSON.isNull("deck")) {
+            suits = DEFAULT_SUITS;
+            ranks = DEFAULT_RANKS;
+            rank_order = DEFAULT_RANK_ORDER;
+        } else {
+            //TODO implement this. Currently only supports default deck.
+            throw new UnsupportedOperationException();
+        }
+        //Gets the teams to use.
+        int[][] teams;
+        if (gameJSON.isNull("teams")) {
+            teams = new int[players][1];
+        } else {
+            teams = convertTeamArray(gameJSON.getJSONArray("teams"));
+        }
+        boolean ascedingOrdering = gameJSON.getBoolean("ascending_ordering");
+        //Rules
+        String calculateScore = null;
+        String trumpPickingMode = null;
+        String trumpSuit = null;
+        String leadingCardForEachTrick = null;
+        String gameEnd = null;
+        Integer scoreThreshold = null;
+        Integer trickThreshold = null;
+        JSONArray rules = gameJSON.getJSONArray("rules"); //TODO check for null
+        for (int i = 0; i < rules.length(); i++) {
+            JSONObject rule = rules.getJSONObject(0);
+            String rulename = rule.getString("name");
+            switch (rulename) {
+                case "calculateScore":
+                    calculateScore = rule.getString("data");
+                    break;
+                case "trumpPickingMode":
+                    trumpPickingMode = rule.getString("data");
+                    break;
+                case "trumpSuit":
+                    trumpSuit = rule.getString("data");
+                    break;
+                case "leadingCardForEachTrick":
+                    leadingCardForEachTrick = rule.getString("data");
+                    break;
+                case "gameEnd":
+                    gameEnd = rule.getString("data");
+                    break;
+                case "scoreThreshold":
+                    scoreThreshold = rule.getInt("data");
+                    break;
+                case "trickThreshold":
+                    trickThreshold = rule.getInt("data");
+                    break;
+                default:
+                    break;
+            }
+        }
+        //Seed for generator
+        long seed = 0xDEADBEEF; //TODO remove this.
+        GameDesc gameDesc = new GameDesc(players,
+                teams,
+                seed,
+                suits,
+                ranks,
+                rank_order,
+                ascedingOrdering,
+                calculateScore,
+                trumpPickingMode,
+                trumpSuit,
+                leadingCardForEachTrick,
+                gameEnd,
+                scoreThreshold,
+                trickThreshold);
+    }
+
+    /**
+     * @param filename Path to file containing the JSON object.
+     * @return JSONObject parsed from the input file.
+     */
+    private JSONObject readJSONFile(String filename) {
+        try (InputStream inputStream = new FileInputStream(filename)) {
+            return new JSONObject(new JSONTokener(inputStream));
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    /**
+     * @param teamsJSON Convert 2 dimension JSON array into a 2 dimensional array of player numbers.
+     * @return
+     */
+    private int[][] convertTeamArray(JSONArray teamsJSON) {
+        int teamSize = teamsJSON.getJSONArray(0).length();
+        int[][] teams = new int[teamsJSON.length()][teamSize];
+        for (int i = 0; i < teamsJSON.length(); i++) {
+            for (int j = 0; j < teamSize; j++) {
+                teams[i][j] = teamsJSON.getJSONArray(i).getInt(j);
+            }
+        }
+        return teams;
+    }
+
 }
 
