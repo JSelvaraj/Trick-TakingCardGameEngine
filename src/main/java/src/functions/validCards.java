67b6ab7be780c0d3f86580d3062d4193a5c6b173
eg207 <eg207@st-andrews.ac.uk>
2019-11-25 22:57:36 +0000

    Updated valid cards to auto check leading cards

--- src/main/java/src/functions/validCards.java
+++ src/main/java/src/functions/validCards.java
@@ -7,13 +7,25 @@ import java.util.function.Predicate;
 
 public class validCards {
 
+    /**
+     * @param nextLegalCardMode Mode for if a card is legal
+     * @param trumpSuit The trump suit. Currently unused, as this doesn't factor in.
+     * @param currentTrick The current trick being played.
+     * @param validLeadingCardPredicate Predicate that checks if a card is a valid leading card. Will be used if the current trick has size 0.
+     * @return Predicate that checks if the card is valid to play.
+     */
     @SuppressWarnings("unused")
-    public static Predicate<Card> getValidCardPredicate(String nextLegalCardMode, String trumpSuit, Hand currentTrick) {
+    public static Predicate<Card> getValidCardPredicate(String nextLegalCardMode, String trumpSuit, Hand currentTrick, Predicate<Card> validLeadingCardPredicate) {
         //TODO work for more than just standard.
-        return (c) -> c.getSUIT().equals(currentTrick.get(0).getSUIT());
+        return (c) -> (currentTrick.getHand().size() == 0 && validLeadingCardPredicate.test(c)) || c.getSUIT().equals(currentTrick.get(0).getSUIT()) ;
     }
 
-    public static Predicate<Card> getValidLeadingCardPredicate(String leadingCardForTrick, String trumpSuit) {
+    /**
+     * @param leadingCardForTrick String containing the criteria for a leading card to be valid
+     * @param trumpSuit The trump suit, can be null.
+     * @return Predicate that checks if a given card is a valid leading card.
+     */
+    public static Predicate<Card> getValidLeadingCardPredicate(String leadingCardForTrick, String trumpSuit) { //TODO update for if trump changes.
         switch (leadingCardForTrick) {
             case "any":
                 return (c) -> true;
