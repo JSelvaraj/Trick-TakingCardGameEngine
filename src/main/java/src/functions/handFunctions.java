711e8f394d49d73d0916bde0d04c6c227cdefd68
eg207 <eg207@st-andrews.ac.uk>
2019-11-29 13:48:02 +0000

    renamed class

--- src/main/java/src/functions/handFunctions.java
+++ src/main/java/src/functions/handFunctions.java
@@ -3,7 +3,7 @@ package src.functions;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
-public class handSize {
+public class handFunctions {
     Supplier<Integer> getHandSize(int initialHandSize, String handSizeMode){
         AtomicInteger handSize = new AtomicInteger(initialHandSize);
         switch (handSizeMode){
