f124ecb34f3bd607a960c0d4891fc57e7ab712e1
pdh5 <pdh5@st-andrews.ac.uk>
2020-02-11 17:25:30 +0000

    rdm

--- src/main/java/src/main.java
+++ src/main/java/src/main.java
@@ -3,8 +3,14 @@
  */
 package src;
 
+import org.json.JSONObject;
 import src.exceptions.InvalidGameDescriptionException;
+import src.gameEngine.GameEngine;
 import src.networking.Networking;
+import src.parser.GameDesc;
+import src.parser.Parser;
+import src.player.LocalPlayer;
+import src.player.Player;
 
 import java.util.Scanner;
 
@@ -16,18 +22,18 @@ public class main {
     public static void main(String[] args) throws InvalidGameDescriptionException {
 
 
-//        System.out.println(new main().getGreeting());
+        System.out.println(new main().getGreeting());
 
-//        switch (args.length) {
-//            case 1: // if hosting only argument should be game description file directory
-//                Networking.hostGame(args[0]);
-//                break;
-//            case 2: // if connecting to another player
-//                Networking.connectToGame(args[0], Integer.parseInt(args[1]));
-//
-//        }
-        System.out.println("test");
-        Scanner scanner = new Scanner(System.in);
+        /*switch (args.length) {
+            case 1: // if hosting only argument should be game description file directory
+                Networking.hostGame(args[0]);
+                break;
+            case 2: // if connecting to another player
+                Networking.connectToGame(args[0], Integer.parseInt(args[1]));
+
+        }*/
+
+        /*Scanner scanner = new Scanner(System.in);
         System.out.println("Local Port to use?");
         int localPort = scanner.nextInt();
         System.out.println("Host or join?\nh(ost)\nj(oin)");
@@ -45,15 +51,13 @@ public class main {
                 break;
             default:
                 throw new IllegalArgumentException();
-        }
+        }*/
 
+        Parser parser = new Parser();
+        JSONObject GameJSON = Parser.readJSONFile("Games/whist.json");
+        GameDesc gameDesc = parser.parseGameDescription(GameJSON);
 
-//        Parser parser = new Parser();
-//        JSONObject GameJSON = Parser.readJSONFile(args[0]);
-//        GameDesc gameDesc = parser.parseGameDescription(GameJSON);
-////        System.out.println(gameDesc);
-//
-//        Player[] playerArray = {new LocalPlayer(0), new LocalPlayer(1), new LocalPlayer(2), new LocalPlayer(3)};
-//        GameEngine.main(gameDesc, 0, playerArray);
+        Player[] playerArray = {new LocalPlayer(0), new LocalPlayer(1), new LocalPlayer(2), new LocalPlayer(3)};
+        GameEngine.main(gameDesc, 3, playerArray,123);
     }
 }
