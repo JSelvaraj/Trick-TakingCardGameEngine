294214d4fa9088702c6159364754cb6d71a0379e
eg207 <eg207@st-andrews.ac.uk>
2020-02-19 10:46:54 +0000

    added trick history storage

--- src/main/java/src/deck/Trick.java
+++ src/main/java/src/deck/Trick.java
@@ -7,11 +7,13 @@ import java.util.List;
 public class Trick {
     private final Card winningCard;
     private final String trumpSuit;
+    private final int winningPlayer;
     private List<Card> trick;
 
-    public Trick(Card winningCard, String trumpSuit, List<Card> trick) {
+    public Trick(Card winningCard, String trumpSuit, int winningPlayer, List<Card> trick) {
         this.winningCard = winningCard;
         this.trumpSuit = trumpSuit;
+        this.winningPlayer = winningPlayer;
         this.trick = trick;
     }
 
@@ -25,5 +27,18 @@ public class Trick {
 
     public List<Card> getTrick() {
         return trick;
+    }
+
+    @Override
+    public String toString() {
+        return "Trick{" +
+                "winningCard=" + winningCard +
+                ", trumpSuit='" + trumpSuit + '\'' +
+                ", trick=" + trick.toString() +
+                '}';
+    }
+
+    public int getWinningPlayer() {
+        return winningPlayer;
     }
 }
