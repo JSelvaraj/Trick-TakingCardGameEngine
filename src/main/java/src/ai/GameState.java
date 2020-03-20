bcdb619c0938daaf37312ff671c01a0ded5a2d8b
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-03-20 15:50:18 +0000

    added initial version of gamestate

--- src/main/java/src/ai/GameState.java
+++ src/main/java/src/ai/GameState.java
@@ -1,7 +1,33 @@
 package src.ai;
 
+import src.gameEngine.Hand;
+
 /**
  * Class to encapsulate the game state at a particular turn
  */
 public class GameState {
+    private Hand deck;
+    private Hand currentTrick;
+    private PlayerState[] playerStates;
+
+    public GameState(Hand deck, int playerCount, int initialHandSize) {
+        this.deck = deck;
+        currentTrick = new Hand();
+        playerStates = new PlayerState[playerCount];
+        for (int i = 0; i < playerStates.length; i++) {
+            playerStates[i] = new PlayerState(i, initialHandSize);
+        }
+    }
+
+    public Hand getDeck() {
+        return deck;
+    }
+
+    public Hand getCurrentTrick() {
+        return currentTrick;
+    }
+
+    public PlayerState[] getPlayerStates() {
+        return playerStates;
+    }
 }
