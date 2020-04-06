13770633466916ec62d079c9ba8f7403bbdd3c2a
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-04-06 13:45:11 +0100

    changed card comparatot to just be a map

---
+++ src/main/java/src/ai/TrickSimulator.java
@@ -0,0 +1,15 @@
+package src.ai;
+
+import src.card.Card;
+
+import java.util.List;
+
+public class TrickSimulator {
+    private List<Card> cards;
+    
+    private 
+
+    private int evaluateWinner(){
+        return 0;
+    }
+}
