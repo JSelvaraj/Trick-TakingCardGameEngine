b00624f1a5d27e9a10a85899d55dcb2510db7705
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-04-06 11:32:49 +0100

    made GameState cloneable

--- src/main/java/src/ai/GameState.java
+++ src/main/java/src/ai/GameState.java
@@ -1,11 +1,12 @@
 package src.ai;
 
+import src.card.Card;
 import src.gameEngine.Hand;
 
 /**
  * Class to encapsulate the game state at a particular turn
  */
-public class GameState {
+public class GameState implements Cloneable{
     private Hand deck;
     private Hand currentTrick;
     private PlayerState[] playerStates;
@@ -19,6 +20,12 @@ public class GameState {
         }
     }
 
+    public GameState(Hand deck, Hand currentTrick, PlayerState[] playerStates) {
+        this.deck = deck;
+        this.currentTrick = currentTrick;
+        this.playerStates = playerStates;
+    }
+
     public Hand getDeck() {
         return deck;
     }
@@ -30,4 +37,18 @@ public class GameState {
     public PlayerState[] getPlayerStates() {
         return playerStates;
     }
+
+    public GameState updateGameState(int playernumber, Card card){
+        GameState newGameState = this.clone();
+        newGameState.currentTrick.getCard(card);
+        newGameState.getPlayerStates()[playernumber].addCard(card);
+        return newGameState;
+    }
+
+    @Override
+    public GameState clone(){
+        return new GameState(deck.clone(), currentTrick.clone(), playerStates.clone());
+    }
+
+
 }
