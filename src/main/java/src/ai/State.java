6210963e4a0f30dd2982890c5b2ed08852b036c5
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-04-09 11:40:52 +0100

    moved generating of belief state to State

--- src/main/java/src/ai/State.java
+++ src/main/java/src/ai/State.java
@@ -1,12 +1,10 @@
 package src.ai;
 
 import src.card.Card;
+import src.deck.Shuffle;
 import src.gameEngine.Hand;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class State {
     private List<Card> deck;
@@ -26,5 +24,29 @@ public class State {
 
     public List<List<Card>> getPlayerHands() {
         return playerHands;
+    }
+
+    /**
+     * Generate a possible state based on a game observation
+     *
+     * @param history The knowledge known about a game.
+     * @return A possible game state based on the observation.
+     */
+    public static State generateBeliefState(GameObservation history, Shuffle shuffle) {
+        State state = new State(history.getDeck(), history.getPlayerObservations().size());
+        //Add the initial cards we know that each player has.
+        for (int i = 0; i < state.getPlayerHands().size(); i++) {
+            state.getPlayerHands().get(i).addAll(history.getPlayerObservations().get(i).getCardsPlayed());
+        }
+        //Shuffle the deck
+        shuffle.shuffle(state.getDeck());
+        Iterator<Card> deckIterator = state.getDeck().iterator();
+        //Iterate over each player, until each player has the correct number of cards.
+        for (int i = 0; i < state.getPlayerHands().size(); i++) {
+            while (state.getPlayerHands().get(i).size() < history.getPlayerObservations().get(i).getCardsLeft()) {
+                state.getPlayerHands().get(i).add(deckIterator.next());
+            }
+        }
+        return state;
     }
 }
