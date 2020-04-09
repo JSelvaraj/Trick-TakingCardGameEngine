80a509f7f46883606407176107795c10db49f262
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-04-09 09:53:57 +0100

    Renamed PlayerState to PlayerObservation

--- src/main/java/src/ai/GameObservation.java
+++ src/main/java/src/ai/GameObservation.java
@@ -6,24 +6,30 @@ import src.gameEngine.Hand;
 /**
  * Class to encapsulate the game state at a particular turn
  */
-public class GameObservation implements Cloneable{
+public class GameObservation implements Cloneable {
+    private int round;
     private Hand deck;
+    private Hand cardsRemaining;
     private Hand currentTrick;
-    private PlayerState[] playerStates;
+    private PlayerObservation[] playerObservations;
 
     public GameObservation(Hand deck, int playerCount, int initialHandSize) {
+        round = 0;
         this.deck = deck;
         currentTrick = new Hand();
-        playerStates = new PlayerState[playerCount];
-        for (int i = 0; i < playerStates.length; i++) {
-            playerStates[i] = new PlayerState(i, initialHandSize);
+        cardsRemaining = deck.clone();
+        playerObservations = new PlayerObservation[playerCount];
+        for (int i = 0; i < playerObservations.length; i++) {
+            playerObservations[i] = new PlayerObservation(i, initialHandSize);
         }
     }
 
-    public GameObservation(Hand deck, Hand currentTrick, PlayerState[] playerStates) {
+    public GameObservation(Hand deck, Hand currentTrick, Hand cardsRemaining, PlayerObservation[] playerObservations, int round) {
         this.deck = deck;
         this.currentTrick = currentTrick;
-        this.playerStates = playerStates;
+        this.cardsRemaining = cardsRemaining;
+        this.playerObservations = playerObservations;
+        this.round = round;
     }
 
     public Hand getDeck() {
@@ -34,20 +40,32 @@ public class GameObservation implements Cloneable{
         return currentTrick;
     }
 
-    public PlayerState[] getPlayerStates() {
-        return playerStates;
+    public PlayerObservation[] getPlayerObservations() {
+        return playerObservations;
     }
 
-    public GameObservation updateGameState(int playernumber, Card card){
+    public GameObservation updateGameState(int playernumber, Card card) {
         GameObservation newGameObservation = this.clone();
         newGameObservation.currentTrick.getCard(card);
-        newGameObservation.getPlayerStates()[playernumber].addCard(card);
+        newGameObservation.getPlayerObservations()[playernumber].addCard(card);
         return newGameObservation;
     }
 
+    public void incrementRound(){
+        round++;
+    }
+
+    public int getRound() {
+        return round;
+    }
+
+    public Hand getCardsRemaining() {
+        return cardsRemaining;
+    }
+
     @Override
-    public GameObservation clone(){
-        return new GameObservation(deck.clone(), currentTrick.clone(), playerStates.clone());
+    public GameObservation clone() {
+        return new GameObservation(deck.clone(), currentTrick.clone(), cardsRemaining.clone(), playerObservations.clone(), round);
     }
 
 
