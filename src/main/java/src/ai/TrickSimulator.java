6520e051fb92291806c40361b3a3ab6e131f0a78
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-04-09 11:31:00 +0100

    Overhauled to used lists instead of arrays, and stopped using clone

--- src/main/java/src/ai/TrickSimulator.java
+++ src/main/java/src/ai/TrickSimulator.java
@@ -8,16 +8,22 @@ import src.card.CardComparator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * Class to simulate a trick being played.
+ */
 public class TrickSimulator {
     private List<Pair<Card, Integer>> trick;
     private CardComparator cardComparator;
+    String trumpSuit;
+    AtomicBoolean breakflag;
 
-    public TrickSimulator(Map<String, Integer> suitOrder) {
+    public TrickSimulator(Map<String, Integer> suitOrder, String trumpSuit) {
         this.trick = new ArrayList<>();
         this.cardComparator = new CardComparator(suitOrder);
     }
-
+    
     public int evaluateWinner() {
         Pair<Card, Integer> currentWinner = trick.get(0);
         Pair<Card, Integer> card;
