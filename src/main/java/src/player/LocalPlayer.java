7afaa761cfd0fe2349fabe0aba7e17ee9a3ba8a8
Jeffrey <selvarajjeffrey@yahoo.co.uk>
2020-04-12 02:19:07 +0100

    Added GUIplayer class

--- src/main/java/src/player/LocalPlayer.java
+++ src/main/java/src/player/LocalPlayer.java
@@ -14,29 +14,29 @@ import java.util.function.Predicate;
 public class LocalPlayer extends Player {
     //Text colours.
     static final String ANSI_RESET = "\u001B[0m";
-    private static final String ANSI_BLACK = "\u001B[30m";
-    private static final String ANSI_RED = "\u001B[31m";
-    private static final String ANSI_GREEN = "\u001B[32m";
-    private static final String ANSI_YELLOW = "\u001B[33m";
-    private static final String ANSI_BLUE = "\u001B[34m";
-    private static final String ANSI_PURPLE = "\u001B[35m";
-    private static final String ANSI_CYAN = "\u001B[36m";
-    private static final String ANSI_WHITE = "\u001B[37m";
-    private static final String[] text_colours = {ANSI_RED, ANSI_YELLOW, ANSI_BLUE, ANSI_PURPLE, ANSI_CYAN, ANSI_GREEN}; //TODO add more colours
+    protected static final String ANSI_BLACK = "\u001B[30m";
+    protected static final String ANSI_RED = "\u001B[31m";
+    protected static final String ANSI_GREEN = "\u001B[32m";
+    protected static final String ANSI_YELLOW = "\u001B[33m";
+    protected static final String ANSI_BLUE = "\u001B[34m";
+    protected static final String ANSI_PURPLE = "\u001B[35m";
+    protected static final String ANSI_CYAN = "\u001B[36m";
+    protected static final String ANSI_WHITE = "\u001B[37m";
+    protected static final String[] text_colours = {ANSI_RED, ANSI_YELLOW, ANSI_BLUE, ANSI_PURPLE, ANSI_CYAN, ANSI_GREEN}; //TODO add more colours
     //background colours
-    private static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
-    private static final String ANSI_RED_BACKGROUND = "\u001B[41m";
-    private static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
-    private static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
-    private static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
-    private static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
-    private static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
-    private static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
-    private static final String[] background_colours = {ANSI_BLACK_BACKGROUND, ANSI_RED_BACKGROUND, ANSI_GREEN_BACKGROUND, ANSI_YELLOW_BACKGROUND, ANSI_BLUE_BACKGROUND, ANSI_PURPLE_BACKGROUND, ANSI_CYAN_BACKGROUND, ANSI_WHITE_BACKGROUND};
+    protected static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    protected static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    protected static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    protected static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    protected static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    protected static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    protected static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    protected static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+    protected static final String[] background_colours = {ANSI_BLACK_BACKGROUND, ANSI_RED_BACKGROUND, ANSI_GREEN_BACKGROUND, ANSI_YELLOW_BACKGROUND, ANSI_BLUE_BACKGROUND, ANSI_PURPLE_BACKGROUND, ANSI_CYAN_BACKGROUND, ANSI_WHITE_BACKGROUND};
 
 //    private static Iterator<String> colours = Arrays.asList(text_colours).iterator();
 
-    private String colour;
+    protected String colour;
 
     public LocalPlayer(int playerNumber, Predicate<Card> validCard) {
         super(playerNumber, validCard);
