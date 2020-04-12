e6882eac42f844c2b976a5bc120ad01ca0387490
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-04-12 12:35:33 +0100

    added test class for search tree

---
+++ src/test/java/src/ai/POMCPTreeNodeTest.java
@@ -0,0 +1,16 @@
+package src.ai;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class POMCPTreeNodeTest {
+
+    @Test
+    void addNode() {
+    }
+
+    @Test
+    void findNode() {
+    }
+}
