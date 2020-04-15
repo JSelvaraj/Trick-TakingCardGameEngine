abbed1c7b9faa338cf41d2fc5da2a2f476ae4de6
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-04-15 11:10:40 +0100

    changed main to use POMDP player

--- src/main/java/src/gameEngine/GameEngine.java
+++ src/main/java/src/gameEngine/GameEngine.java
@@ -10,6 +10,7 @@ import src.functions.validCards;
 import src.parser.GameDesc;
 import src.player.LocalPlayer;
 import src.player.NetworkPlayer;
+import src.player.POMDPPlayer;
 import src.player.Player;
 import src.rdmEvents.RdmEvent;
 import src.rdmEvents.RdmEventsManager;
@@ -461,7 +462,7 @@ public class GameEngine {
             }
         } else { //Only need to print out network moves to local players
             for (Player player : playerArray) {
-                if (player.getClass() == LocalPlayer.class) {
+                if (player.getClass() == LocalPlayer.class || player.getClass() == POMDPPlayer.class) {
                     player.broadcastPlay(card, playerNumber);
                 }
             }
