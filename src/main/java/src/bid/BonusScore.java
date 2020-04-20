06ba6da619f0178b7490f353b4e028b52d495757
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-04-20 11:58:16 +0100

    added handscore

--- src/main/java/src/bid/BonusScore.java
+++ src/main/java/src/bid/BonusScore.java
@@ -4,7 +4,16 @@ public abstract class BonusScore {
     private int bonusScore;
     private boolean vulnerable;
 
+    public BonusScore(int bonusScore, boolean vulnerable) {
+        this.bonusScore = bonusScore;
+        this.vulnerable = vulnerable;
+    }
+
     public boolean matches(Bid bid, int handScore, int tricks) {
         return bid.isVulnerable() == vulnerable;
+    }
+
+    public int getBonusScore() {
+        return bonusScore;
     }
 }
--- src/main/java/src/bid/handScore.java
+++ src/main/java/src/bid/handScore.java
@@ -1,4 +1,18 @@
 package src.bid;
 
 public class handScore extends BonusScore {
+    private int handScoreMin;
+    private int handScoreMax;
+
+
+    public handScore(int bonusScore, boolean vulnerable, int handScoreMin, int handScoreMax) {
+        super(bonusScore, vulnerable);
+        this.handScoreMin = handScoreMin;
+        this.handScoreMax = handScoreMax;
+    }
+
+    @Override
+    public boolean matches(Bid bid, int handScore, int tricks) {
+        return super.matches(bid, handScore, tricks) && handScore >= handScoreMin && handScore <= handScoreMax;
+    }
 }
