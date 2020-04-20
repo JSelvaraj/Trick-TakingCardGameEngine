1f57f22ce5b26a7ffad9f09d328e17ad06e9cdc5
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-04-20 11:55:24 +0100

    added initial matching method

--- src/main/java/src/bid/BonusScore.java
+++ src/main/java/src/bid/BonusScore.java
@@ -3,4 +3,8 @@ package src.bid;
 public abstract class BonusScore {
     private int bonusScore;
     private boolean vulnerable;
+
+    public boolean matches(Bid bid, int handScore, int tricks) {
+        return bid.isVulnerable() == vulnerable;
+    }
 }
