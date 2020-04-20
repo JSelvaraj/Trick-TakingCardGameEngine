2cd54e711129bbf867edec4f6735b8b767a56bf8
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-04-20 11:59:57 +0100

    added game bonus

--- src/main/java/src/bid/GameBonus.java
+++ src/main/java/src/bid/GameBonus.java
@@ -1,4 +1,19 @@
 package src.bid;
 
 public class GameBonus extends BonusScore {
+    int tricks;
+
+    public GameBonus(int bonusScore, boolean vulnerable, int tricks) {
+        super(bonusScore, vulnerable);
+        this.tricks = tricks;
+    }
+
+    @Override
+    public boolean matches(Bid bid, int handScore, int tricks) {
+        return super.matches(bid, handScore, tricks) && tricks == this.tricks;
+    }
+
+    public int getTricks() {
+        return tricks;
+    }
 }
