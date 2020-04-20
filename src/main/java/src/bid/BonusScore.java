05cd19fae5e15ec44a9b79b51c9b1ad8076b764b
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-04-20 11:53:40 +0100

    added bonus score classes

---
+++ src/main/java/src/bid/BonusScore.java
@@ -0,0 +1,6 @@
+package src.bid;
+
+public abstract class BonusScore {
+    private int bonusScore;
+    private boolean vulnerable;
+}
---
+++ src/main/java/src/bid/GameBonus.java
@@ -0,0 +1,4 @@
+package src.bid;
+
+public class GameBonus extends BonusScore {
+}
