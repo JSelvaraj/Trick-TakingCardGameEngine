1ff3b023e9bf01d294db3c3cae67c058857cee8e
ph-git <p.higgins.mail@gmail.com>
2020-04-26 16:12:05 +0200

    commenting and reviewing random events

--- src/main/java/src/rdmEvents/RdmEventsManager.java
+++ src/main/java/src/rdmEvents/RdmEventsManager.java
@@ -11,45 +11,57 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.function.Predicate;
 
+//Class that distributes and runs the logic of random events.
 public class RdmEventsManager {
-
+    //Defines the maximum score separation between the weakest and strongest team that is allowed before
+    //the manager increases the chance of a random event occurring.
     int maxAcceptableScoreSeparation;
-    int scoreThreshold;
+    //Probability between 0 and 1 that a random event will be triggered
     double rdmEventProb;
-    final double rdmEventProbDEFAULT = 0.4;
-    double probIncrement = 0.3;
-    ArrayList<Team> teams;
+    //Default probability - after a random event is run, probability resets to this
+    final double rdmEventProbDEFAULT;
+    //Value by which the probability is increased when the maxAcceptableScoreSeparation is breached
+    double probIncrement;
+    //Flag to indicate if random events are triggered
+    boolean enabled;
+    //Score threshold as specified in game description
+    int scoreThreshold;
+    //Stores the weakest (lowest-scoring) and strongest team (highest-scoring)
     Team weakestTeam;
     Team strongestTeam;
-    boolean enabled;
-    Random rand;
-    Player[] players;
+    //Array to choose which special card to insert
     String[] specialCardEvents = {"BOMB", "HEAVEN"};
+    //Array to choose which trick event to run
     String[] TRICKEvents = {"SwapCard", "SwapHands"};
+    Random rand;
+    ArrayList<Team> teams;
+    Player[] players;
     GameDesc desc;
 
+    //Constructor to initialise the random event manager
     public RdmEventsManager(GameDesc desc, ArrayList<Team> teams, Random rand, Player[] players, boolean enabled) {
         this.teams = teams;
         this.enabled = enabled;
         this.rand = rand;
         this.players = players;
         this.desc = desc;
-        setup();
-    }
-
-    public void setup() {
-        rdmEventProb = rdmEventProbDEFAULT;
+        //Set initial weak/strong teams
         setWeakestTeam(teams.get(0));
         setStrongestTeam(teams.get(1));
         scoreThreshold = desc.getScoreThreshold();
+        //Set max score separation based on the game desc
         maxAcceptableScoreSeparation = scoreThreshold / 3;
+        //Starting probability a random event is run
+        rdmEventProbDEFAULT = 0.2;
+        rdmEventProb = rdmEventProbDEFAULT;
+        probIncrement = 0.3;
     }
 
-
+    //Method that checks the team scores and raises the random event probability of necessary
     public void checkGameCloseness() {
         int highestScore = 0;
         int lowestScore = scoreThreshold + 1;
-
+        //Find weakest and strongest team
         for (Team team : teams) {
             if (team.getGameScore() < lowestScore) {
                 weakestTeam = team;
@@ -60,9 +72,9 @@ public class RdmEventsManager {
                 highestScore = team.getGameScore();
             }
         }
-
+        //Check if allowed score separation is exceeded
         if (highestScore - lowestScore > maxAcceptableScoreSeparation) {
-            System.out.println("Balancing needed");
+            //Increment the random probability or set to max if necessary
             if ((rdmEventProb += probIncrement) <= 1) {
                 rdmEventProb += probIncrement;
             } else {
@@ -71,11 +83,16 @@ public class RdmEventsManager {
         }
     }
 
+    //Method that decides if a random event should be run, and chooses one if necessary
     public RdmEvent eventChooser(String eventPlayTime) {
+        //If random events are enabled, randomly decide if an event should be run
         if (enabled && rand.nextDouble() < rdmEventProb) {
+            //Choose an appropriate event based on the point in the game
             switch (eventPlayTime) {
                 case "TRICK":
+                    //Reset the chance of a random event
                     rdmEventProb = rdmEventProbDEFAULT;
+                    //Choose the event to be run and return it to the game engine
                     return new RdmEvent(TRICKEvents[rand.nextInt(TRICKEvents.length)]);
                 case "SPECIAL-CARD":
                     rdmEventProb = rdmEventProbDEFAULT;
@@ -88,14 +105,16 @@ public class RdmEventsManager {
         }
     }
 
+    //Method that runs the logic for a swap cards event
     public void runSwapCards() {
         Player[] playerArray = getPlayers();
-        int rdmPlayerIndexFrTeam = getRand().nextInt(getWeakestTeam().getPlayers().length);
-        Player weakPlayer = getWeakestTeam().getPlayers()[rdmPlayerIndexFrTeam];
-
+        //Randomly select a player from the weakest team
+        int rdmPlayerIndexFromWeakTeam = getRand().nextInt(getWeakestTeam().getPlayers().length);
+        Player weakPlayer = getWeakestTeam().getPlayers()[rdmPlayerIndexFromWeakTeam];
+        //Randomly select a player from the strongest team
         int rdmStrongPlayerTeamIndex = getRand().nextInt(getStrongestTeam().getPlayers().length);
         Player rdmStrongPlayer = strongestTeam.getPlayers()[rdmStrongPlayerTeamIndex];
-
+        //Ask the weak player if the want to swap a card with the random player
         Swap swap = weakPlayer.getSwap(rdmStrongPlayer);
         if (swap.getStatus().equals("live")) {
             Card originalPlayerCard = playerArray[swap.getOriginalPlayerIndex()].getHand().giveCard(swap.getOriginalPlayerCardNumber());
@@ -149,14 +168,6 @@ public class RdmEventsManager {
         int rdmCardIndex = getRand().nextInt(desc.getHandSize());
         playerArray[rdmPlayerIndex].getHand().get(rdmCardIndex).setSpecialType(rdmEventHAND.getName());
         System.out.println(playerArray[rdmPlayerIndex].getHand().get(rdmCardIndex) + " is a " + rdmEventHAND.getName() + " special card.");
-    }
-
-    public int getMaxAcceptableScoreSeparation() {
-        return maxAcceptableScoreSeparation;
-    }
-
-    public void setMaxAcceptableScoreSeparation(int maxAcceptableScoreSeparation) {
-        this.maxAcceptableScoreSeparation = maxAcceptableScoreSeparation;
     }
 
     public Team getWeakestTeam() {
