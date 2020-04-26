639c11b12a4050bfa5325f3fbbc67e160c2c01a6
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-04-26 20:44:15 +0100

    added deck to game description

--- src/main/java/src/parser/GameDesc.java
+++ src/main/java/src/parser/GameDesc.java
@@ -55,6 +55,7 @@ public class GameDesc {
     private int minBid;
     private int maxBid;
     private List<String> bidSuits;
+    private Deck deck;
 
 
     /**
@@ -93,7 +94,8 @@ public class GameDesc {
                     boolean canBidBlind,
                     int minBid,
                     int maxBid,
-                    List<String> bidSuits) {
+                    List<String> bidSuits,
+                    Deck deck) {
         this.name = name;
         this.NUMBEROFPLAYERS = numOfPlayers;
         this.teams = teams;
@@ -126,6 +128,7 @@ public class GameDesc {
         this.minBid = minBid;
         this.maxBid = maxBid;
         this.bidSuits = bidSuits;
+        this.deck = deck;
     }
 
     @Override
@@ -156,7 +159,7 @@ public class GameDesc {
     }
 
     public LinkedList<Card> getDECK() {
-        return Deck.makeDeck(this.SUITS, this.RANKS);
+        return this.deck.cards;
     }
 
     public boolean isDEALCARDSCLOCKWISE() {
