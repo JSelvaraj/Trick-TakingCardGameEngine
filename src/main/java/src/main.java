3b2509d9d59019b1497021101230cd4f86939e5d
Ewan Gilligan <eg207@st-andrews.ac.uk>
2020-04-29 20:27:43 +0100

    removed property setting

--- src/main/java/src/main.java
+++ src/main/java/src/main.java
@@ -46,7 +46,6 @@ public class main {
     }
 
     public static void main(String[] args) {
-        System.setProperty("java.net.preferIPv4Stack", "false");
         CommandHost host = new CommandHost();
         CommandJoin join = new CommandJoin();
 
